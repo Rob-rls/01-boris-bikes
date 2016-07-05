092a7b0fe7b58b34a03b641e6820283bfc062afa
Robert Summers <rsummers02@gmail.com>
2016-07-05 21:36:31 +0100

    ch10 - still in progress

--- lib/boris-bike.rb
+++ lib/boris-bike.rb
@@ -1,8 +1,9 @@
+require './lib/bike'
+
 class DockingStation
 
-  
   def release_bike
-
+    Bike.new
   end
 
 end
--- spec/boris-bike_spec.rb
+++ spec/boris-bike_spec.rb
@@ -6,6 +6,11 @@ describe DockingStation do
     expect(docking_station.respond_to?(:release_bike)).to eq true
   end
 
+  it 'is expected to new instance of a Bike when the release_bike method is called' do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to be_a_kind_of(Bike)
+  end
+
   #it { is_expected(DockingStation.respond_to?(:release_bike)).to eq true }
 
 end
