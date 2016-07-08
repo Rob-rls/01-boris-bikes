833fa21cd5afdd119b62072a623496083d4bf8db
Robert Summers <rsummers02@gmail.com>
2016-07-08 09:31:58 +0100

    refactored

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,8 @@ describe Bike do
   it "should respond to 'working?'" do
     expect(subject).to respond_to :working
   end
+
+  it 'shoud be able to update the working status' do
+    expect(subject.working = false).to eq false
+  end
 end
